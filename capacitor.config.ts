cc7c9a1b38d4d427aefc2ff9f972087a5ff2d53b
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-04-03 14:54:54 -0500

    add ios and android platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'issue-53333',
-  webDir: 'www'
+  webDir: 'www',
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;

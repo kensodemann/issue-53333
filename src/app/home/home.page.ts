6a3a156d1e1f87d1a5382aba35f12de973431f29
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-04-03 15:57:15 -0500

    add some inputs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonInput, IonItem, IonList, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, IonHeader, IonInput, IonItem, IonList, IonTitle, IonToolbar],
 })
-export class HomePage {
-  constructor() {}
-}
+export class HomePage {}
